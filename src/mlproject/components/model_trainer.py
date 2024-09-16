c982ac354684233be70a0746720e4383f021fc5c
abhisek-13 <abhisekmaharana9861@gmail.com>
2024-09-16 21:49:08 +0530

    experiment tracking and model monitoring successful.

--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -1,12 +1,16 @@
 import os
 import sys
+import numpy as np
+from urllib.parse import urlparse
+import mlflow
+import dagshub
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 from sklearn.linear_model import LinearRegression,Lasso,Ridge
 from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor,GradientBoostingRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
 from dataclasses import dataclass
 from src.mlproject.utils import evaluate_models,save_object
 
@@ -17,6 +21,14 @@ class ModelTrainerConfig:
 class ModelTrainer:
   def __init__(self):
     self.model_trainer_config = ModelTrainerConfig()
+    
+    
+  def eval_metrics(self,actual,pred):
+    rmse = np.sqrt(mean_squared_error(actual,pred))
+    mae = mean_absolute_error(actual,pred)
+    r2 = r2_score(actual,pred)
+    
+    return rmse,mae,r2
     
   def initiate_model_trainer(self,train_array,test_array):
     try:
@@ -64,6 +76,43 @@ class ModelTrainer:
       best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
       
       best_model = models[best_model_name]
+      
+      print("This is the best model:")
+      print(best_model_name)
+      
+      model_names = list(params.keys())
+      actual_name = ""
+      
+      for model in model_names:
+        if model == best_model_name:
+          actual_name += model
+      
+      best_params = params[actual_name]
+      
+      mlflow.set_registry_uri("https://dagshub.com/abhisek-13/MLops.mlflow")
+      tracking_url_type_score = urlparse(mlflow.get_tracking_uri()).scheme
+      
+      
+      dagshub.init(repo_owner='abhisek-13', repo_name='MLops', mlflow=True)
+      
+      # mlflow
+      with mlflow.start_run():
+        predicted_qualities = best_model.predict(x_test)
+        
+        rmse,mae,r2 = self.eval_metrics(y_test,predicted_qualities)
+        
+        mlflow.log_param("Best Parameter",best_params)
+        
+        mlflow.log_metric("rmse",rmse)
+        mlflow.log_metric("mae",mae)
+        mlflow.log_metric("r2",r2)
+      
+        if tracking_url_type_score != "file":
+          mlflow.sklearn.log_model(best_model,"model",registered_model_name = actual_name)
+        else:
+          mlflow.sklearn.log_model(best_model,"model")
+      
+      
       
       if best_model_score < 0.6:
         raise CustomException("No best model found")
